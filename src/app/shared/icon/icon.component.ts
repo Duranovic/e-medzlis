ca9dd61608f0b69620f913c1a7a14cf9fd000f12
Velid Duranovic <vduranov@cisco.com>
2022-08-29 14:20:56 +0200

    Created icon component

---
+++ src/app/shared/icon/icon.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iz-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconComponent implements OnInit {
+  @Input() name: string;
+  @Input() size: string;
+  @Input() color: string;
+  @Input() stroke: string;
+  @Input() active: boolean;
+  @Input() disableHover: boolean;
+
+  public link: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+    this.link = `assets/images/${this.name}.svg#${this.name}`
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IconComponent } from './icon/icon.component';
+import { InputSearchComponent } from './input-search/input-search.component';
+import { TableComponent } from './table/table.component';
 
 
 
 @NgModule({
   declarations: [
-    InputComponent
+    InputComponent,
+    IconComponent,
+    InputSearchComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
   ],
   exports: [
-    InputComponent
+    InputComponent,
+    IconComponent,
+    InputSearchComponent,
+    TableComponent
   ]
 })
 export class SharedModule { }
