4053a24d377bc269a5f105945df8b575b11b4c06
Velid Duranovic <velidprivate@gmail.com>
2024-02-18 11:32:39 +0100

    Replacing hardcoded values with real one

--- src/app/features/clanovi/pages/clanovi-evidentiraj-modal/clanovi-evidentiraj-modal.component.ts
+++ src/app/features/clanovi/pages/clanovi-evidentiraj-modal/clanovi-evidentiraj-modal.component.ts
@@ -1,12 +1,19 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import {Observable, take} from 'rxjs';
 import { Clan } from 'src/app/core/models/clan.model';
 import { StoreService } from 'src/app/core/services/store.service';
 import { SnackbarService } from 'src/app/core/services/snackbar.service';
 import { TableService } from 'src/app/core/services/table.service';
 import { PlacanjeVM } from '../../models/placanje.model';
-import { snackbarEvidentionError, snackbarEvidentionSuccess } from '../../constants/placanje.constants';
+import {
+  evidentionConstant,
+  snackbarEvidentionError,
+  snackbarEvidentionSuccess
+} from '../../constants/placanje.constants';
+import {Korisnik} from "../../../../core/models/korisnik.model";
+import {map} from "rxjs/operators";
+import {Clanarina} from "../../../../core/models/clanarina.model";
 
 @Component({
   selector: 'app-clanovi-evidentiraj-modal',
@@ -17,18 +24,24 @@ import { snackbarEvidentionError, snackbarEvidentionSuccess } from '../../consta
 export class ClanoviEvidentirajModalComponent implements OnInit {
   public clan$: Observable<Clan>;
   public evidention: PlacanjeVM;
+  public clanarina$: Observable<Clanarina | undefined>;
 
-
-  constructor(public dialogRef: MatDialogRef<ClanoviEvidentirajModalComponent>, private snackbarService: SnackbarService ,private storeService: StoreService, private tableService: TableService) { }
+  constructor(public store: StoreService, dialogRef: MatDialogRef<ClanoviEvidentirajModalComponent>, private snackbarService: SnackbarService, private tableService: TableService) { }
 
   public ngOnInit(): void {
-    this.clan$ = this.storeService.selectedClan$;
+    this.clan$ = this.store.selectedClan$;
     this.evidention = this.tableService.selectedElement;
+    this.clanarina$ = this.store.clanarine$.pipe(
+      map(clanarine =>
+        clanarine.find(clanarina => clanarina.godina === this.evidention.for_year)
+      ),
+      take(1),
+    )
   }
 
   public evidentiraj() {
     if(this.evidention.clan_id) {
-      this.storeService.evidentiraj(this.evidention.clan_id, this.evidention.for_year);
+      this.store.evidentiraj(this.evidention.clan_id, this.evidention.for_year);
       this.snackbarService.openSnackbarSuccess(snackbarEvidentionSuccess.title, snackbarEvidentionSuccess.description);
     }
     else {
@@ -36,4 +49,14 @@ export class ClanoviEvidentirajModalComponent implements OnInit {
       console.error("clan_id is undefined");
     }
   }
+
+  public getOvlastenaOsoba(korisnik: Korisnik): string {
+      return `${korisnik.first_name} ${korisnik.last_name}`;
+  }
+
+  public getDate(): string {
+    return new Date().toLocaleDateString('de-DE');
+  }
+
+  protected readonly evidentionConstant = evidentionConstant;
 }
